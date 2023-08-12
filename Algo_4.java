11f51b9d87aa676a071d7f15c8642cf01cedbb21
Achinda <achindamihiruk@gmail.com>
2023-08-12 19:38:04 +0530

    Completed Task 4

---
+++ Algo_4.java
@@ -0,0 +1,19 @@
+public class Algo_4 {
+    public static void main(String[] args) {
+
+        int[] nums = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < (nums.length/2); i++) {
+            int temp = 0;
+            temp = nums[nums.length-(i+1)];
+            nums[nums.length-(i+1)] = nums[i];
+            nums[i] = temp;
+        }
+        
+        System.out.print("[ ");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+ ", ");
+        }
+        System.out.print("\b\b ]\n");
+    }
+}
