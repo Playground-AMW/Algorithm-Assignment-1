30be376d47b3265521034751d2062777123dcb73
Achinda <achindamihiruk@gmail.com>
2023-08-14 01:14:00 +0530

    Completed Task 8.3

--- Algo_8_3.java
+++ Algo_8_3.java
@@ -1,12 +1,9 @@
-public class Algo_8_2 {
+public class Algo_8_3 {
     public static void main(String[] args) {
-
+        
         int[] numA = { 5, 7, -2, 3, 4, 6, -8 };
         int[] numB = { 7, 8, -8, 2, 1, -9, 6 };
 
-        for (int i : numA) {
-            System.out.print(i + ", ");
-        }
         myLoop:
         for (int i = 0; i < numA.length; i++) {
             
@@ -14,7 +11,7 @@ public class Algo_8_2 {
                 if (numA[i] == numB[j]) continue myLoop;
                     
             }
-            System.out.print(numB[i] + ", ");
+            System.out.print(numA[i] + ", ");
         }
         System.out.println("\b\b ");
     }
